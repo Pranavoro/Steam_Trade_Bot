b94de99ab3780f2b8b17284f2e6afad267048b3b
Pranavoro <45733558+Pranavoro@users.noreply.github.com>
2019-06-03 10:00:40 +0530

    Create config.js
    This file is just to keep ourselves a little organised.

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = 
+{
+	username:       'Your_username',
+	password:       'Your_password',
+	sharedSecret:   'Your_shared_secret',
+	identitySecret: 'Your_identity_secret',
+	SteamID:        'Your_steam_ID',
+}
